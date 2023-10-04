62873d1fb168415349ccad4325d85288e85b8746
윤동휘 <xorms5712@gmail.com>
2023-10-04 23:38:13 +0900

    231004

---
+++ 202310_1st_week/baekjoon_14891_gears.py
@@ -0,0 +1,17 @@
+# baekjoon 14891번 톱니바퀴
+
+# 시작되는 톱니바퀴와 회전방향을 입력받아 톱니바퀴들을 회전시키는 함수
+def rotation(n, d):
+    global gears
+
+
+
+
+
+
+
+gears = [list(map(int, input())) for _ in range(4)]
+K = int(input())
+for _ in range(K):
+    N, D = map(int, input())
+    
