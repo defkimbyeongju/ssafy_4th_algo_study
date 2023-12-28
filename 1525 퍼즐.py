06c789611101fd2d0728071d34ab487c52b9c7a5
Sanghyeop Lee <139518057+Riemanntensortotoro@users.noreply.github.com>
2023-12-28 14:32:43 +0900

    Add files via upload

--- 1525 퍼즐.py
+++ 1525 퍼즐.py
@@ -1,48 +1,89 @@
+# from collections import deque
+
+# def is_valid(x, y):
+#     return 0 <= x < 3 and 0 <= y < 3
+
+# def is_equal(board1, board2):
+#     for i in range(3):
+#         for j in range(3):
+#             if board1[i][j] != board2[i][j]:
+#                 return False
+#     return True
+
+# def BFS(board):
+#     target = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+#     count = 0
+#     queue = deque([(board, count)])
+#     visited = set()
+
+#     while queue:
+#         current, count = queue.popleft()
+
+#         if is_equal(current, target):
+#             return count
+
+#         # 이전에 방문한 상태를 문자열로 변환하여 집합에 추가
+#         visited.add(''.join(map(str, current)))
+
+#         for i in range(3):
+#             for j in range(3):
+#                 if current[i][j] == 0:
+#                     x, y = i, j
+
+#         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+#             nx, ny = x + dx, y + dy
+
+#             if is_valid(nx, ny):
+#                 next_board = [row[:] for row in current]
+#                 next_board[x][y], next_board[nx][ny] = next_board[nx][ny], next_board[x][y]
+
+#                 # 이전에 방문한 상태가 아니라면 큐에 추가
+#                 if ''.join(map(str, next_board)) not in visited:
+#                     queue.append((next_board, count + 1))
+
+#     return -1
+
+# board = [list(map(int, input().split())) for _ in range(3)]
+# result = BFS(board)
+# print(result)
+
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-def to_2d_list(s):
-    return [list(map(int, s[i:i+3])) for i in range(0, 9, 3)]
+def get_str(board):
+    return ''.join(str(num) if num != 0 else '9' for row in board for num in row)
 
-def to_string(board):
-    return ''.join(map(str, [num for row in board for num in row]))
+def bfs(start):
+    target = '123456789'
+    queue = deque([(start, 0)])
+    visited = {start}
 
-def is_target(board):
-    return board == [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-
-def swap(board, i1, j1, i2, j2):
-    new_board = [row[:] for row in board]
-    new_board[i1][j1], new_board[i2][j2] = new_board[i2][j2], new_board[i1][j1]
-    return new_board
-
-def get_neighbors(board, i, j):
-    neighbors = []
-    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        if 0 <= i + di < 3 and 0 <= j + dj < 3:
-            neighbors.append(swap(board, i, j, i + di, j + dj))
-    return neighbors
-
-def bfs(start_board):
-    queue = deque([(start_board, 0)])
-    visited = set([to_string(start_board)])
     while queue:
-        current_board, moves = queue.popleft()
+        current, count = queue.popleft()
 
-        if is_target(current_board):
-            return moves
+        if current == target:
+            return count
 
-        i, j = next((i, j) for i in range(3) for j in range(3) if current_board[i][j] == 0)
+        zero_pos = current.index('9')
+        x, y = divmod(zero_pos, 3)
 
-        for neighbor in get_neighbors(current_board, i, j):
-            neighbor_str = to_string(neighbor)
-            if neighbor_str not in visited:
-                visited.add(neighbor_str)
-                queue.append((neighbor, moves + 1))
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 3 and 0 <= ny < 3:
+                nidx = nx * 3 + ny
+                swap_board = list(current)
+                swap_board[zero_pos], swap_board[nidx] = swap_board[nidx], swap_board[zero_pos]
+                swap_str = ''.join(swap_board)
+
+                if swap_str not in visited:
+                    visited.add(swap_str)
+                    queue.append((swap_str, count + 1))
+
     return -1
 
-# 입력 받기
-input_string = ''.join(["".join(input().split()) for _ in range(3)])
-start_board = to_2d_list(input_string)
+board = [list(map(int, input().split())) for _ in range(3)]
+start = get_str(board)
 
-# BFS 실행
-result = bfs(start_board)
+result = bfs(start)
 print(result)
