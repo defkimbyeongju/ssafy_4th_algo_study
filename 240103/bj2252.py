e0546845932b78280796ba6dd1c323d00163abf3
hhheeeeee <libravickyyy@gmail.com>
2024-01-03 23:14:49 +0900

    240123

---
+++ 240103/bj2252.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('test.txt')
+input = sys.stdin.readline
+from collections import deque
+
+# n 명이 학생, m개의 비교
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+in_degree = [0 for _ in range(n+1)]
+queue = deque()
+answer = []
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    in_degree[b] += 1
+
+for i in range(1, n+1):
+    if in_degree[i] == 0:
+        queue.append(i)
+
+while queue:
+    now = queue.popleft()
+    answer.append(now)
+    for next in graph[now]:
+        in_degree[next] -= 1
+        if in_degree[next] == 0:
+            queue.append(next)
+
+print(*answer)
+
