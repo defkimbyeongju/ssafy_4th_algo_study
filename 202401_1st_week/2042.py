8704806bb2d990ee370b90ff89e2e063a22cce47
윤동휘 <xorms1119@naver.com>
2024-01-03 22:04:26 +0900

    240103

--- 202401_1st_week/2042.py
+++ 202401_1st_week/2042.py
@@ -1,7 +1,21 @@
 # 2042 구간 합 구하기
+import sys
+
+
+def change_num(b, c):
+    num_dict[b] = c
+
+
+def sum_num(b, c):
+    total = 0
+    for i in range(b, c + 1):
+        total += num_dict[i]
+
+    print(total)
+
 
 # N = 수의 갯수, M = 수 변경 횟수, K = 구간합 구하는 횟수
-N, M, K = map(int, input().split())
+N, M, K = map(int, sys.stdin.readline().split())
 
 # 숫자들 기록할 dict
 num_dict = {}
@@ -10,6 +24,12 @@ num_dict = {}
 for i in range(N):
     num_dict[i + 1] = int(input())
 
-print(num_dict)
+# 행동 입력받기
+for _ in range(M + K):
+    A, B, C = map(int, sys.stdin.readline().split())
+    if A == 1:
+        change_num(B, C)
+    else:
+        sum_num(B, C)
 
 
