ee796e3367a2e7c0fe3936b1d64ce8c4ceaf0400
윤동휘 <xorms1119@naver.com>
2024-02-27 17:57:53 +0900

    240227

--- 202402_5th_week/baekjoon_11725.py
+++ 202402_5th_week/baekjoon_11725.py
@@ -1,1 +1,20 @@
- 
+# 트리의 부모 찾기
+
+# BFS
+def BFS():
+    
+
+
+
+# 노드 갯수
+N = int(input())
+
+# 인접리스트
+line_list = [[] for i in range(N + 1)]
+
+# 인접리스트 정보 입력
+for i in range(N - 1):
+    a, b = int(input().split())
+    line_list[a].append(b)
+    line_list[b].append(a)
+
